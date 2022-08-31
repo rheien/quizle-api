c7c55da43d2835739fff67ed90cf844863e5c3af
Ryan Le <ryan.le@axelspringer.com>
2022-08-31 14:04:15 +0200

    refactoring handler

--- src/main/java/org/example/quizleapi/QuizleApi.java
+++ src/main/java/org/example/quizleapi/QuizleApi.java
@@ -2,16 +2,20 @@ package org.example.quizleapi;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandler;
-import org.example.quizleapi.game.QuestionHandler;
+import org.example.quizleapi.api.QuestionController;
+import org.example.quizleapi.business.QuestionService;
+import org.example.quizleapi.business.RandomQuestionService;
 
 //setting a server
-public class JettyServer {
+public class QuizleApi {
 
     public static void main(String[] args) throws Exception {
 
+        QuestionService questionService = new RandomQuestionService();
+
         final ContextHandler context = new ContextHandler();
-        context.setContextPath("/api/v1/questions");
-        context.setHandler(new QuestionHandler());
+        context.setContextPath(QuestionController.CONTEXT_PATH);
+        context.setHandler(new QuestionController(questionService));
 
         final String port = System.getenv().getOrDefault("PORT", "8888");
         final Server server = new Server(Integer.parseInt(port));
--- src/main/java/org/example/quizleapi/api/QuestionController.java
+++ src/main/java/org/example/quizleapi/api/QuestionController.java
@@ -1,5 +1,7 @@
 package org.example.quizleapi.api;
 
+import org.example.quizleapi.business.QuestionService;
+
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
@@ -8,9 +10,15 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDateTime;
 
-public class QuestionHandler extends AbstractHandler {
+public class QuestionController extends AbstractHandler {
 
     public static final String CONTEXT_PATH = "/api/v1/questions";
+
+    private final QuestionService questionService;
+
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
 
     @Override
     public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException {
